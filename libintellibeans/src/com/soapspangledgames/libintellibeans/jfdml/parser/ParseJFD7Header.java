1d29cb77e4c38cde67ed86430cec8af44e5e30bd
Soap Spangled Games <libintellibeans@soapspangledgames.com>
2021-01-27 11:29:37 -0600

    Created the ParseJFDM7Header class skeleton.

---
+++ libintellibeans/src/com/soapspangledgames/libintellibeans/jfdml/parser/ParseJFD7Header.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2020 by Soap Spangled Games.
+ * All Rights Reserved.
+ *
+ * No portion of this software may be copied, distributed, or modified without
+ * the express written permission of the copyright holder.
+ */
+package com.soapspangledgames.libintellibeans.jfdml.parser;
+
+/**
+ *
+ * @author Soap Spangled Games
+ */
+public class ParseJFD7Header
+	{
+	private final String m_stLine;
+	
+	public ParseJFD7Header(String stLine)
+		{
+		m_stLine = stLine;
+		}
+	}
