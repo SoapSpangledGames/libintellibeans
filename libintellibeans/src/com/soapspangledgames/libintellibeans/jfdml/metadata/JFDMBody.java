802a087e902c1048a10dd45b2eccab8704d1dcf0
Soap Spangled Games <libintellibeans@soapspangledgames.com>
2021-01-27 15:40:50 -0600

    Added the JFDMBody class.

---
+++ libintellibeans/src/com/soapspangledgames/libintellibeans/jfdml/metadata/JFDMBody.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2020 by Soap Spangled Games.
+ * All Rights Reserved.
+ *
+ * No portion of this software may be copied, distributed, or modified without
+ * the express written permission of the copyright holder.
+ */
+package com.soapspangledgames.libintellibeans.jfdml.metadata;
+
+/**
+ *
+ * @author Soap Spangled Games
+ */
+public class JFDMBody
+	{
+	public JFDMBody()
+		{
+		
+		}
+	}
