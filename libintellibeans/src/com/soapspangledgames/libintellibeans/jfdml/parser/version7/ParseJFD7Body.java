4be63784fc4f5c3a4328110ab5f3009a712cbbd7
Soap Spangled Games <libintellibeans@soapspangledgames.com>
2021-01-27 15:40:40 -0600

    Added the JFDMBody class.

---
+++ libintellibeans/src/com/soapspangledgames/libintellibeans/jfdml/parser/version7/ParseJFD7Body.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2020 by Soap Spangled Games.
+ * All Rights Reserved.
+ *
+ * No portion of this software may be copied, distributed, or modified without
+ * the express written permission of the copyright holder.
+ */
+package com.soapspangledgames.libintellibeans.jfdml.parser.version7;
+
+import com.soapspangledgames.libintellibeans.jfdml.exceptions.JFDMParseError;
+import com.soapspangledgames.libintellibeans.jfdml.metadata.JFDMBody;
+
+/**
+ *
+ * @author Soap Spangled Games
+ */
+public class ParseJFD7Body
+	{
+	private final JFDMBody m_body = new JFDMBody();
+	
+	private final String m_stData;
+	
+	private int m_nOffset;
+	
+	public ParseJFD7Body(String stData, int nOffset)
+		{
+		m_stData = stData;
+		m_nOffset = nOffset;
+		}
+	
+	public JFDMBody getBody()
+		{
+		return m_body;
+		}
+	
+	public void parse() throws JFDMParseError
+		{
+		}
+	}
