fdd48ed148d4434fc60c58550d91eb3798de5091
Soap Spangled Games <libintellibeans@soapspangledgames.com>
2021-01-27 11:09:38 -0600

    Added the JFDMParseError exception class.

---
+++ libintellibeans/src/com/soapspangledgames/libintellibeans/jfdml/exceptions/JFDMParseError.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2020 by Soap Spangled Games.
+ * All Rights Reserved.
+ *
+ * No portion of this software may be copied, distributed, or modified without
+ * the express written permission of the copyright holder.
+ */
+package com.soapspangledgames.libintellibeans.jfdml.exceptions;
+
+/**
+ *
+ * @author Soap Spangled Games
+ */
+public class JFDMParseError extends Exception
+	{
+	public JFDMParseError(String msg)
+		{
+		super(msg);
+		}
+	}
