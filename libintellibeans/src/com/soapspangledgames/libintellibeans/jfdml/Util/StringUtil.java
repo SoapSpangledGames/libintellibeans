0b9381c6fdb8895a808384ba8ef9390ac7c250f9
Soap Spangled Games <libintellibeans@soapspangledgames.com>
2021-01-27 16:22:09 -0600

    Added the StringUtil class.

---
+++ libintellibeans/src/com/soapspangledgames/libintellibeans/jfdml/Util/StringUtil.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2020 by Soap Spangled Games.
+ * All Rights Reserved.
+ *
+ * No portion of this software may be copied, distributed, or modified without
+ * the express written permission of the copyright holder.
+ */
+package com.soapspangledgames.libintellibeans.jfdml.Util;
+
+/**
+ *
+ * @author Soap Spangled Games
+ */
+public class StringUtil
+	{
+	public StringUtil()
+		{
+		
+		}
+	
+	static public int skipWhiteSpace(String stData, int nOffset)
+		{
+		int ret = nOffset;
+		
+		if (stData != null && nOffset >= 0)
+			{
+			if (StringUtil.isWhiteSpace(stData.charAt(nOffset)))
+				{
+				++nOffset;
+				}
+			}
+		return ret;
+		}
+	
+	static public boolean isWhiteSpace(char c)
+		{
+		return c < '!' || c > '~';
+		}
+	}
