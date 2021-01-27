5fa45ecaff0c5a9b9a7758286be188286ab5da27
Soap Spangled Games <libintellibeans@soapspangledgames.com>
2021-01-27 10:56:50 -0600

    Created the JFD7Parser class.

---
+++ libintellibeans/src/com/soapspangledgames/libintellibeans/jfdml/parser/JFD7Parser.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2020 by Soap Spangled Games.
+ * All Rights Reserved.
+ *
+ * No portion of this software may be copied, distributed, or modified without
+ * the express written permission of the copyright holder.
+ */
+package com.soapspangledgames.libintellibeans.jfdml.parser;
+
+/**
+ * This parses version 7 of the IntelliJ Form Designer Markup Language, and
+ * creates the corresponding Java metadata that will be used to create the
+ * correponding Netbeans form.
+ * 
+ * @author Soap Spangled Games
+ */
+public class JFD7Parser
+	{
+	private final String m_stData;
+	
+	public JFD7Parser(String stData)
+		{
+		m_stData = stData;
+		}
+	
+	public void parse()
+		{
+		
+		}
+	}
