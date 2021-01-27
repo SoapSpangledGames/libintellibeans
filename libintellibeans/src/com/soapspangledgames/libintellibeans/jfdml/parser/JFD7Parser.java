fce46642a152fa1a6f5e477449be824b149c2e69
Soap Spangled Games <libintellibeans@soapspangledgames.com>
2021-01-27 11:22:36 -0600

    Can now read the entire file through main(), and start the parsing process. Can parse the identifier.

--- libintellibeans/src/com/soapspangledgames/libintellibeans/jfdml/parser/JFD7Parser.java
+++ libintellibeans/src/com/soapspangledgames/libintellibeans/jfdml/parser/JFD7Parser.java
@@ -7,6 +7,13 @@
  */
 package com.soapspangledgames.libintellibeans.jfdml.parser;
 
+import com.soapspangledgames.libintellibeans.jfdml.exceptions.JFDMParseError;
+import com.soapspangledgames.libintellibeans.jfdml.metadata.JFDMHeader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 /**
  * This parses version 7 of the IntelliJ Form Designer Markup Language, and
  * creates the corresponding Java metadata that will be used to create the
@@ -16,7 +23,11 @@ package com.soapspangledgames.libintellibeans.jfdml.parser;
  */
 public class JFD7Parser
 	{
+	private JFDMHeader m_header = new JFDMHeader();
+	
 	private final String m_stData;
+	
+	private int m_nOffset;
 	
 	/**
 	 * 
@@ -27,8 +38,38 @@ public class JFD7Parser
 		m_stData = stData;
 		}
 	
-	public void parse()
+	public void parse() throws JFDMParseError
 		{
+		m_nOffset = 0;
+		parseHeader();
+		}
+	
+	private void parseHeader() throws JFDMParseError
+		{
+		parseIdentifier();
+		}
+	
+	private void parseIdentifier() throws JFDMParseError
+		{
+		int nTemp = m_stData.indexOf(" ", m_nOffset);
 		
+		if (nTemp != -1)
+			{
+			m_header.m_stIdentifier = m_stData.substring(m_nOffset, nTemp);
+			
+			m_nOffset += nTemp + 1;
+			}
+		else
+			{
+			throw new JFDMParseError("Could not find the JFDM identifier at offset " + m_nOffset);
+			}
+		}
+	
+	public static void main(String[] args) throws IOException, JFDMParseError
+		{
+		List<String> lstLines = Files.readAllLines(Paths.get("/home/personal/temp/test.jfdm"));
+		JFD7Parser parser = new JFD7Parser(lstLines.toString().replace("[", "").replace("]", ""));
+		
+		parser.parse();
 		}
 	}
