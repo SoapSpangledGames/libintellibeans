c9b72a27f49768fcf314a7e4d213d5e75f391446
stormreaver <stormreaver@github.com>
2021-01-27 09:48:34 -0600

    Initial commit to version control.

---
+++ libintellibeans/src/libintellibeans/Libintellibeans.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2020 by Anthony H. O'Bryan.
+ * All Rights Reserved.
+ *
+ * No portion of this software may be copied, distributed, or modified without
+ * the express written permission of the copyright holder.
+ */
+package libintellibeans;
+
+/**
+ *
+ * @author personal
+ */
+public class Libintellibeans
+	{
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args)
+		{
+		// TODO code application logic here
+		}
+	
+	}
