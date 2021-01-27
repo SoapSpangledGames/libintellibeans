5e990dae92ca7d61b11ced8b1cc3f854051174fe
Soap Spangled Games <libintellibeans@soapspangledgames.com>
2021-01-27 11:07:16 -0600

    Make constructor public.

---
+++ libintellibeans/src/com/soapspangledgames/libintellibeans/jfdml/metadata/JFDMHeader.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2020 by Soap Spangled Games.
+ * All Rights Reserved.
+ *
+ * No portion of this software may be copied, distributed, or modified without
+ * the express written permission of the copyright holder.
+ */
+package com.soapspangledgames.libintellibeans.jfdml.metadata;
+
+/**
+ *
+ * @author Soap Spangled Games
+ */
+public class JFDMHeader
+	{
+	public String m_stIdentifier;
+	
+	public String m_stProgram;
+	
+	public String m_stProgramVersion;
+	
+	public String m_stJavaVersion;
+	
+	public String m_stEncoding;
+	
+	public JFDMHeader()
+		{
+		
+		}
+	}
