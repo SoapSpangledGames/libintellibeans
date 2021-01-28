96c3b496f77ef9d961e35221bb8d2b0e3631bda3
Soap Spangled Games <libintellibeans@soapspangledgames.com>
2021-01-28 09:18:11 -0600

    Created the skeletal ContentType class.

---
+++ libintellibeans/src/com/soapspangledgames/libintellibeans/jfdml/metadata/ContentType.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2020 by Soap Spangled Games.
+ * All Rights Reserved.
+ *
+ * No portion of this software may be copied, distributed, or modified without
+ * the express written permission of the copyright holder.
+ */
+package com.soapspangledgames.libintellibeans.jfdml.metadata;
+
+/**
+ *
+ * @author Soap Spangled Games
+ */
+public class ContentType
+	{
+	public String m_stRawType;
+	
+	public ContentType()
+		{
+		
+		}
+	}
