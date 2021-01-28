525a6db9ec739f78c9e01fd3a0a3312a5a4a3da5
Soap Spangled Games <libintellibeans@soapspangledgames.com>
2021-01-28 10:07:38 -0600

    Renamed the JFDMFormRoot class to JFDMFormModelRoot.

---
+++ libintellibeans/src/com/soapspangledgames/libintellibeans/jfdml/parser/version7/JFDMFormModelRoot.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2020 by Soap Spangled Games.
+ * All Rights Reserved.
+ *
+ * No portion of this software may be copied, distributed, or modified without
+ * the express written permission of the copyright holder.
+ */
+package com.soapspangledgames.libintellibeans.jfdml.parser.version7;
+
+/**
+ *
+ * @author Soap Spangled Games
+ */
+public class JFDMFormModelRoot
+	{
+	public JFDMFormModelRoot()
+		{
+		
+		}
+	}
