8e75b3ea03e1237fe3accd7bd94c129374a4df7b
Soap Spangled Games <libintellibeans@soapspangledgames.com>
2021-01-28 08:58:46 -0600

    Created the initial ParseFormModel class.

---
+++ libintellibeans/src/com/soapspangledgames/libintellibeans/jfdml/parser/version7/ParseFormModel.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2020 by Soap Spangled Games.
+ * All Rights Reserved.
+ *
+ * No portion of this software may be copied, distributed, or modified without
+ * the express written permission of the copyright holder.
+ */
+package com.soapspangledgames.libintellibeans.jfdml.parser.version7;
+
+import com.soapspangledgames.libintellibeans.jfdml.exceptions.JFDMParseError;
+
+/**
+ *
+ * @author Soap Spangled Games
+ */
+public class ParseFormModel
+	{
+	private final String m_stData;
+
+	private int m_nOffset;
+
+	public ParseFormModel(String stData)
+		{
+		m_stData = stData;
+		}
+
+	public void parse(int nOffset) throws JFDMParseError
+		{
+		
+		}
+	}
