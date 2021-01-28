f34ce7f9b8d2982673ca768ce24cae2a0adb725d
Soap Spangled Games <libintellibeans@soapspangledgames.com>
2021-01-28 08:48:33 -0600

    Detect the FormModel object type in the body.

--- libintellibeans/src/com/soapspangledgames/libintellibeans/jfdml/parser/version7/ParseJFD7Body.java
+++ libintellibeans/src/com/soapspangledgames/libintellibeans/jfdml/parser/version7/ParseJFD7Body.java
@@ -18,46 +18,63 @@ import com.soapspangledgames.libintellibeans.jfdml.metadata.JFDMBody;
 public class ParseJFD7Body
 	{
 	private final JFDMBody m_body = new JFDMBody();
-	
+
 	private final String m_stData;
-	
+
 	private int m_nOffset;
-	
+
 	public ParseJFD7Body(String stData)
 		{
 		m_stData = stData;
 		}
-	
+
 	public JFDMBody getBody()
 		{
 		return m_body;
 		}
-	
+
 	public void parse(int nOffset) throws JFDMParseError
 		{
 		int nTemp;
-		
+
 		m_nOffset = StringUtil.skipWhiteSpace(m_stData, nOffset);
 		nTemp = StringUtil.findNextWhiteSpace(m_stData, m_nOffset);
 		while (nTemp > m_nOffset)
 			{
-			if ( (nTemp = StringUtil.findNextWhiteSpace(m_stData, m_nOffset)) != -1)
+			String stCommand = m_stData.substring(m_nOffset, nTemp).toLowerCase();
+
+			switch (stCommand)
 				{
-				String stCommand = m_stData.substring(m_nOffset, nTemp).toLowerCase();
-				
-				switch (stCommand)
-					{
-					case "new":
-						m_nOffset = nTemp;
-						processNewCommand();
-						break;
-					}
+				case "new":
+					m_nOffset = nTemp;
+					processNewCommand();
+					break;
 				}
 			m_nOffset = nTemp;
+			nTemp = StringUtil.findNextWhiteSpace(m_stData, m_nOffset);
+			}
+		}
+
+	private void processNewCommand()
+		{
+		int nTemp;
+
+		m_nOffset = StringUtil.skipWhiteSpace(m_stData, m_nOffset);
+		nTemp = StringUtil.findNextWhiteSpace(m_stData, m_nOffset);
+		while (nTemp > m_nOffset)
+			{
+			String stType = m_stData.substring(m_nOffset, nTemp).toLowerCase();
+			
+			switch (stType)
+				{
+				case "formmodel":
+					processFormModel();
+					break;
+				}
 			}
 		}
 	
-	public void processNewCommand()
+	private void processFormModel()
 		{
 		
 		}
